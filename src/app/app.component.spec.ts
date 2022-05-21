9d17d2d8f59286d7237032252035499cdd0e7de2
matijasevicstipe2 <105871487+matijasevicstipe2@users.noreply.github.com>
2022-05-21 02:55:41 +0200

    Commit src

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'nghard'`, () => {
+  it(`should have as title 'hardwareapp-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('nghard');
+    expect(app.title).toEqual('hardwareapp-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('nghard app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('hardwareapp-frontend app is running!');
   });
 });
